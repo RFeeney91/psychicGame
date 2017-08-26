a3c5e838a4289ab695c4d732ad51b04b215d4296
RFeeney91 <ryansfeeney@gmail.com>
2017-08-25 17:29:27 -0700

    fixJS

--- psychicGame/assets/javascript/game.js
+++ psychicGame/assets/javascript/game.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var guesses = [
 "A", 
 "B", 
@@ -29,35 +28,3 @@ var guesses = [
 ];
 
 console.log(guesses)
-=======
-var guesses = [
-"A", 
-"B", 
-"C", 
-"D", 
-"E", 
-"F", 
-"G", 
-"H", 
-"I", 
-"J", 
-"K", 
-"L", 
-"M", 
-"N", 
-"O", 
-"P", 
-"Q", 
-"R", 
-"S", 
-'T', 
-"U", 
-"V", 
-"W", 
-"X", 
-"Y", 
-"Z" 
-];
-
-console.log(guesses)
->>>>>>> 612ff9869b2516513b1a1e380a3c788606c23146
